9994b09259767a352260afe9f7757e5eef5447ae
Irvin SAmboy <contacoirvinsamboy@gmail.com>
2025-04-14 11:40:44 -0400

    creating initial form

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 export default function App() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <header className="bg-blue-600 py-6 max-h-72">
+        <h1 className="uppercase text-center font-black text-4xl text-white"> 
+          Expense Planning
+        </h1>
+      </header>
+      <div className="p-4 max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 pt-10">
+        <p>hi</p>
+      </div>
+    </>
   )
 }
---
+++ src/controllers/BudgetForm.tsx
@@ -0,0 +1,10 @@
+export default function BudgetForm() {
+
+  return (
+    <form action="">
+        <label htmlFor="budget">Define budget</label>
+        <input type="number" placeholder="Define your budget" />
+        <input type="submit" placeholder="Define budget" />
+    </form>
+  )
+}
