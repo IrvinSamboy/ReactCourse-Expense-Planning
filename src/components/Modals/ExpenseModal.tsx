f61e01acd936a4e3768d7c0118bc95016536335c
Irvin SAmboy <contacoirvinsamboy@gmail.com>
2025-04-15 16:08:26 -0400

    creating close modal function

--- src/components/Modals/ExpenseModal.tsx
+++ src/components/Modals/ExpenseModal.tsx
@@ -1,8 +1,20 @@
-export default function ExpenseModal({visible} : {visible: boolean}) {
-  return (
+type ExpenseModalProps = {
+    visible: boolean
+    closeModal: (value: boolean) => void
+}
+
+export default function ExpenseModal({visible, closeModal} : ExpenseModalProps) {
+
+    const handleChangeVisibility = (e : React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+        e.stopPropagation()
+        closeModal(false)
+    }
+
+    return (
     <div 
         className={`size-full bg-black/40 flex items-center 
                     justify-center absolute right-0 top-0 ${visible&& "hidden"}`}
+        onClick={handleChangeVisibility}
     >
         <div className="p-4 max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 w-full pt-10">
         </div>
