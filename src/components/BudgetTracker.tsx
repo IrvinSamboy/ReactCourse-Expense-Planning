33334ea1d8ddab2f7a4d4b7c4ac1991827b3eaf0
Irvin SAmboy <contacoirvinsamboy@gmail.com>
2025-04-22 10:52:16 -0400

    getting started with expense form

--- src/components/BudgetTracker.tsx
+++ src/components/BudgetTracker.tsx
@@ -32,7 +32,7 @@ export default function BudgetTracker() {
     </div>
     <div 
     className="absolute bottom-4 text-white text-6xl 
-              font-bold right-4 bg-blue-600 px-5 py-2.5 flex items-center 
+              font-bold right-4 bg-blue-600 px-4 py-1.5 flex items-center 
               justify-center rounded-full cursor-pointer"
     onClick={() => setVisible(true)}
     >
--- src/components/Modals/ExpenseModal.tsx
+++ src/components/Modals/ExpenseModal.tsx
@@ -16,7 +16,47 @@ export default function ExpenseModal({visible, closeModal} : ExpenseModalProps) 
                     justify-center absolute right-0 top-0 ${!visible&& "hidden"}`}
         onClick={handleCloseModal}
     >
-        <div className="p-4 max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 w-full pt-10">
+        <div className="p-4 max-w-3xl mx-auto space-y-4 bg-white shadow-lg rounded-lg mt-10 w-full pt-10">
+            <h2 className="text-center font-black text-2xl border-b-4 pb-4 border-blue-600">New Expense</h2>
+            <form action="" className="space-y-4">
+            <div className="flex flex-col space-y-2">
+                    <label 
+                        htmlFor=""
+                        className="font-semibold"
+                    >
+                        Expense name
+                    </label>
+                    <input 
+                        className="p-1 bg-gray-200 rounded-sm"
+                        type="text" 
+                    />
+                </div>
+                <div className="flex flex-col space-y-2">
+                    <label 
+                        htmlFor=""
+                        className="font-semibold"
+                    >
+                        Quantity
+                    </label>
+                    <input 
+                        className="p-1 bg-gray-200 rounded-sm"
+                        type="text" 
+                    />
+
+                </div>Category
+                <div className="flex flex-col space-y-2">
+                    <label 
+                        htmlFor=""
+                        className="font-semibold"
+                    >
+                        Category
+                    </label>
+                    <input 
+                        className="p-1 bg-gray-200 rounded-sm"
+                        type="text" 
+                    />
+                </div>
+            </form>
         </div>
     </div>
   )
