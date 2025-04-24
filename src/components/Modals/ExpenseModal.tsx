d71d5dae270157894639f5930a58332322593ebb
Irvin SAmboy <contacoirvinsamboy@gmail.com>
2025-04-24 15:10:34 -0400

    adding categories object for select input

--- src/components/Modals/ExpenseModal.tsx
+++ src/components/Modals/ExpenseModal.tsx
@@ -3,66 +3,84 @@ type ExpenseModalProps = {
     closeModal: (value: boolean) => void
 }
 
-export default function ExpenseModal({visible, closeModal} : ExpenseModalProps) {
+export const categories = [
+    { id: '1', name: 'Savings', icon: 'ahorro' },
+    { id: '2', name: 'Food', icon: 'comida' },
+    { id: '3', name: 'Housing', icon: 'casa' },
+    { id: '4', name: 'Miscellaneous Expenses', icon: 'gastos' },
+    { id: '5', name: 'Leisure', icon: 'ocio' },
+    { id: '6', name: 'Health', icon: 'salud' },
+    { id: '7', name: 'Subscriptions', icon: 'suscripciones' },
+];
 
-    const handleCloseModal = (e : React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+export default function ExpenseModal({ visible, closeModal }: ExpenseModalProps) {
+
+    const handleCloseModal = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
         e.stopPropagation()
         closeModal(false)
     }
 
     return (
-    <div 
-        className={`size-full bg-black/40 flex items-center 
-                    justify-center absolute right-0 top-0 ${!visible&& "hidden"}`}
-        onClick={handleCloseModal}
-    >
-        <div className="p-4 max-w-3xl mx-auto space-y-4 bg-white shadow-lg rounded-lg mt-10 w-full pt-10">
-            <h2 className="text-center font-black text-2xl border-b-4 pb-4 border-blue-600">New Expense</h2>
-            <form action="" className="space-y-4">
-            <div className="flex flex-col space-y-2">
-                    <label 
-                        htmlFor=""
-                        className="font-semibold"
-                    >
-                        Expense name
-                    </label>
-                    <input 
-                        className="p-1 bg-gray-200 rounded-sm"
-                        type="text" 
-                    />
-                </div>
-                <div className="flex flex-col space-y-2">
-                    <label 
-                        htmlFor=""
-                        className="font-semibold"
-                    >
-                        Quantity
-                    </label>
-                    <input 
-                        className="p-1 bg-gray-200 rounded-sm"
-                        type="text" 
-                    />
+        <div
+            className={`size-full bg-black/40 flex items-center 
+                    justify-center absolute right-0 top-0 ${!visible && "hidden"}`}
+            onClick={handleCloseModal}
+        >
+            <div className="p-4 max-w-3xl mx-auto space-y-4 bg-white shadow-lg rounded-lg mt-10 w-full pt-10">
+                <h2 className="text-center font-black text-2xl border-b-4 pb-4 border-blue-600">New Expense</h2>
+                <form action="" className="space-y-4">
+                    <div className="flex flex-col space-y-2">
+                        <label
+                            htmlFor=""
+                            className="font-semibold"
+                        >
+                            Expense name
+                        </label>
+                        <input
+                            className="p-1 bg-gray-200 rounded-sm"
+                            type="text"
+                        />
+                    </div>
+                    <div className="flex flex-col space-y-2">
+                        <label
+                            htmlFor=""
+                            className="font-semibold"
+                        >
+                            Quantity
+                        </label>
+                        <input
+                            className="p-1 bg-gray-200 rounded-sm"
+                            type="text"
+                        />
 
-                </div>Category
-                <div className="flex flex-col space-y-2">
-                    <label 
-                        htmlFor=""
-                        className="font-semibold"
-                    >
-                        Category
-                    </label>
-                    <input 
-                        className="p-1 bg-gray-200 rounded-sm"
-                        type="text" 
+                    </div>Category
+                    <div className="flex flex-col space-y-2">
+                        <label
+                            htmlFor=""
+                            className="font-semibold"
+                        >
+                            Category
+                        </label>
+                        <select name="" id="">
+                            {
+                                categories.map((item) => (
+                                    <option
+                                        key={item.id}
+                                        value={item.id}
+                                    >
+                                        {item.name}
+                                    </option>
+                                ))
+                            }
+                        </select>
+                    </div>
+                    <input
+                        type="submit"
+                        value="Register Exprense"
+                        className="w-full p-2 font-black disabled:opacity-40 bg-blue-600 text-white rounded-sm"
                     />
-                </div>
-                <input
-                    type="submit"
-                    value="Register Exprense"
-                    className="w-full p-2 font-black disabled:opacity-40 bg-blue-600 text-white rounded-sm"
-                />
-            </form>
+                </form>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
