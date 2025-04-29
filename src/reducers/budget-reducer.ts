f4f7eb879e839ef5c0306e1094c0468d227860e9
Irvin Samboy <contacoirvinsamboy@gmail.com>
2025-04-29 12:39:07 -0400

    creating add expense action

--- src/reducers/budget-reducer.ts
+++ src/reducers/budget-reducer.ts
@@ -1,11 +1,17 @@
-export type budgetActions = {type: 'add-budget', payload: {budget: number}}
+import { ExpenseItem, ExpenseState } from "../types/ExpenseModalTypes"
+
+export type budgetActions = 
+    {type: 'add-budget', payload: {budget: number}} |
+    {type: 'add-expense', payload: {expense: ExpenseItem}}
 
 export type budgetState = {
     budget: number
+    expense: ExpenseState[]
 }
 
 export const initialState : budgetState = {
-    budget: 0
+    budget: 0,
+    expense: []
 }
 
 export const budgetReducer = (
@@ -16,6 +22,19 @@ export const budgetReducer = (
         return {
             ...state,
             budget: actions.payload.budget
+        }
+    }
+
+    if(actions.type === "add-expense"){
+        return {
+            ...state,
+            expense: [
+                ...state.expense,
+                {
+                    ...actions.payload.expense,
+                    id: state.expense.length === 0? 0: state.expense[state.expense.length - 1].Id++
+                }
+            ]
         }
     }
 
