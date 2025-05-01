7aa0cab1b254aa224d62550b826fd2e3ded3386b
Irvin Samboy <contacoirvinsamboy@gmail.com>
2025-04-30 23:19:10 -0400

    creating main structure for expense list

---
+++ src/components/ExpenseList.tsx
@@ -0,0 +1,19 @@
+
+export default function ExpenseList() {
+  return (
+    <div>
+        <h2>Expense list</h2>
+        <div>
+            <div>
+                <img src="" alt="" />
+                <div>
+                    <p>SUSCRIPTIONS</p>
+                    <p>Netflix</p>
+                    <p>Monday, december 5 of 2025</p>
+                </div>
+            </div>
+            <p>$100</p>
+        </div>
+    </div>
+  )
+}
